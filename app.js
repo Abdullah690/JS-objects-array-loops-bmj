ab8734c9a0540f0279f6153003b96c6bd9d7660e
Abdullah690 <as250543@gmail.com>
2021-11-23 03:07:54 +0500

    js objects

---
+++ app.js
@@ -0,0 +1,153 @@
+// var names = [];
+// document.write(names);  
+
+// var studentNames = [];
+// studentNames [0] = "abdullah"
+// studentNames [1] = "ahmed"
+// studentNames [2] = "ali"
+// console.log(studentNames);
+
+
+// var age = [""]
+// console.log(age);
+
+
+// var num = [];
+// num [0] = 3;
+// num [1] = 5;
+// num [2] = 7;
+// console.log(num);
+
+
+// var word = [];
+// word [0] = "true";
+// word [1] = "false";
+// word [2] = "null";
+// console.log(word);                                                    
+
+
+// var mixedArray = [];
+// mixedArray [0] = "";
+// mixedArray [1] = 10;
+// mixedArray [2] = true;
+// mixedArray [3] = null;
+// mixedArray [4];
+// console.log(mixedArray);
+
+
+// var qualifications =[];
+// qualifications [0] = "1) SSC";
+// qualifications [1] = " <br> 2) HSC";
+// qualifications [2] = "<br> 3) BSC";
+// qualifications [3] = "<br> 4) BS";
+// qualifications [4] = "<br> 5) BCOM";
+// qualifications [5] = "<br> 6) MS";
+// qualifications [6] = "<br> 7) M.Phil";
+// qualifications [7] = "<br> 8) PhD";
+// document.write(qualifications);
+
+
+// var stdNames = ["michael","john","tony"]
+// var stdResult = ["320","230","480"]
+
+// for(var i=0; i<stdNames.length; i++){
+//     document.write(stdNames[i]+" "+stdResult[i]+"<br>");
+//     if(stdResult[i]>=500){
+//         document.write("<b> "+stdNames[i]+" passed</b>");
+//     }
+ 
+// }
+
+
+// var citiesName =["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// var copycitiesNames = citiesName.slice("2","4");
+// document.write(citiesName +"<br>");
+// document.write(copycitiesNames);
+
+// var countryNames = ["pakistan","india","africa","england","china"];
+// var copycountryNames = countryNames.slice("0","2")
+// console.log(countryNames);
+// console.log(copycountryNames);
+
+
+
+// var sentence = ["This","is","my","cat"];
+// var joinSentence = sentence.join(" ");
+// document.write(joinSentence);
+
+
+// var firstdevices = ["keyboard","mouse","printer","monitor"];
+// document.write(firstdevices);
+// document.write("  <br>  ")
+// document.write("  <br>  ")
+// var devices = ["keyboard","mouse","printer","monitor"];
+// for(var i=0; i<devices.length; i++){
+//     document.write(devices[i]+"<br>")
+// }
+
+
+// var print = ["keyboard","mouse","printer","monitor"];
+// document.write(print);
+// document.write("  <br>  ")
+// document.write("  <br>  ")
+// var system = ["keyboard","mouse","printer","monitor"];
+// var system1 = system.reverse();
+// document.write(system1)
+
+
+
+
+// for(var i=0; i<=10; i++){
+//     document.write(i+"<br>");
+// }
+
+
+// var tableNum = prompt("Enter your table Number");
+// var tableLength = prompt("Enter your table length");
+// document.write("table no: "+" "+tableNum+" "+" "+"table length: "+tableLength+"<br>")
+
+// for(var i=1; i<=tableLength; i++){
+//     document.write(tableNum+" "+"x"+" "+i+" "+"="+" "+tableNum*i+"<br>")
+// }
+
+
+
+//  var bakriItems = ["cake","colddrink","chips","juise","milk"]
+//  var userInput = prompt("Enter your bakri")
+//  var match = true;
+
+//  for(var i=0; i<=bakriItems.length; i++){
+//      if(userInput === bakriItems[i]){
+//          alert("yes sir")
+//          match= false;
+//             break;
+//      }
+//  }
+//  if(match===true){
+//      alert("sorry sir")
+//  }
+
+
+
+// var largestNum = [1,2,3,4,5,6,50,8,9,10];
+// var largest = largestNum[0];
+// for(var i=0; i<largestNum.length; i++){
+//     if(largestNum[i]>largest){
+//         largest = largestNum[i];
+//     }
+// }
+// document.write("largest number is: "+largest);
+
+
+
+
+// var smallestNum = [1,2,3,4,5,6,50,8,9,10];
+// var smallest = smallestNum[0];
+// for(var i=0; i<smallestNum.length; i++){
+//     if(smallestNum[i]<smallest){
+//         smallest = smallestNum[i];
+//     }
+// }
+// document.write("smallest number is: "+smallest);
+
+
